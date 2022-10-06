ef3951f614c75678c476b9a65a054efb134975c3
krisha amatya <kamatya@growbydata.com>
2022-10-06 10:27:11 +0545

    push

---
+++ HackerRankSolution/HackerRankProblemSolution/MinMaxSumArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAArray1
+{
+  public  class MinMaxSumArray
+    {
+        public void MinMaxSumArrays(List<int> arr) {
+
+            var arr1 = arr.Select(i => (long)i).ToArray();
+            Array.Sort(arr1);
+
+            var minimum = arr1.Take(4).Sum();
+            var maximum = arr1.Skip(1).Sum();
+            Console.WriteLine(minimum + " " +maximum);
+           
+
+        }
+    }
+}
---
+++ HackerRankSolution/HackerRankProblemSolution/PlusMinus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAArray1
+{
+   public class PlusMinus
+    {
+        public void FindPlusMinus(List<int> arr) {
+          
+            decimal positiveCount = 0;
+            decimal negativeCount = 0;
+            decimal zeroCount = 0;
+            int arrayLength = arr.Count;
+
+            positiveCount = arr.Count(x => x > 0);
+            negativeCount = arr.Count(x => x < 0);
+            zeroCount = arr.Count(x => x == 0);
+
+            
+            Console.WriteLine((positiveCount / arrayLength).ToString("N6"));
+            Console.WriteLine((negativeCount / arrayLength).ToString("N6"));
+            Console.WriteLine((zeroCount / arrayLength).ToString("N6"));
+        }
+    }
+}
