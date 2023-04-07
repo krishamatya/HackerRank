532a48a2f609595f1a6bb6c82be3c51efbca4473
kamatya <kamatya@styleslogistics.com>
2023-04-07 16:56:17 +0545

    cc

---
+++ SignalRAuth/SignalRAuth/Hub/SignalrHub.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+namespace SignalRAuth
+{
+    public class SignalrHub : Hub
+    {
+        private readonly static ConnectionMapping<string> _connections =
+            new ConnectionMapping<string>();
+
+        public async Task SendMessage(string user, string message)
+        {
+            int d = 123;
+            byte a = (byte)d;
+            for (int sa = 0; sa < a; sa++)
+            {
+
+            }
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            string name = Context.User.Identity.Name;
+           _connections.Add(name, Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+           
+            string name = Context.User.Identity.Name;
+            var cIds = _connections.GetConnections(name).ToList();
+            cIds.ForEach(x => {
+                _connections.Remove(name, x);
+            });
+           
+
+            Context.Abort();
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
